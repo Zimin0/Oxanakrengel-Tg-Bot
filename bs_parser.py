3e96a2b1f319dada321048699f893e3435747965
Zimin0 <you@example.com>
2024-03-08 00:42:55 +0300

    Add type hints

--- bs_parser.py
+++ bs_parser.py
@@ -1,27 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
+from typing import Callable, Any, Dict, Optional
 import time
 import hashlib
 import json
 
 class WebPageParser:
-    def __init__(self, debug=False):
-        self.debug = debug
+    def __init__(self, debug: bool = False) -> None:
+        self.debug: bool = debug
         
-    def time_decorator(func):
-        def wrapper(self, *args, **kwargs):
-            start_time = time.time() if self.debug else None
-            result = func(self, *args, **kwargs)
+    def time_decorator(func: Callable) -> Callable:
+        def wrapper(self: 'WebPageParser', *args: Any, **kwargs: Any) -> Any:
+            start_time: Optional[float] = time.time() if self.debug else None
+            result: Any = func(self, *args, **kwargs)
             if self.debug:
-                end_time = time.time()
+                end_time: float = time.time()
                 print(f"{func.__name__} выполнена за {round(end_time - start_time, 3)} секунд.")
             return result
         return wrapper
 
     @time_decorator
-    def get_html(self, url):
+    def get_html(self, url: str) -> Optional[str]:
         """Получение HTML-контента по URL."""
-        response = requests.get(url)
+        response: requests.Response = requests.get(url)
         if response.status_code == 200:
             return response.text
         else:
@@ -29,12 +30,12 @@ class WebPageParser:
             return None
 
     @time_decorator
-    def parse_html(self, html_content):
+    def parse_html(self, html_content: str) -> Dict[str, Any]:
         """Парсинг HTML и извлечение данных."""
-        soup = BeautifulSoup(html_content, 'html.parser')
-        title = soup.find('h2', class_='flex-box').text.strip()
-        unique_id = hashlib.md5(title.encode('utf-8')).hexdigest()
-        product = {
+        soup: BeautifulSoup = BeautifulSoup(html_content, 'html.parser')
+        title: str = soup.find('h2', class_='flex-box').text.strip()
+        unique_id: str = hashlib.md5(title.encode('utf-8')).hexdigest()
+        product: Dict[str, Any] = {
             "id": unique_id,
             "title": title,
             "price": soup.find('div', class_='cart-info__price').text.strip(),
@@ -44,22 +45,22 @@ class WebPageParser:
         }
         return product
 
-    def save_to_json(self, data):
+    def save_to_json(self, data: Dict[str, Any]) -> None:
         """Сохранение данных о продукте в JSON файл с уникальным именем."""
-        filename = f"product_info_{data['id']}.json"
+        filename: str = f"product_info_{data['id']}.json"
         with open(filename, 'w', encoding='utf-8') as f:
             json.dump(data, f, ensure_ascii=False, indent=4)
         if self.debug: print(f'Информация о продукте "{data["title"]}" сохранена в файл {filename}.')
 
-    def run(self, url, save_to_file=False):
-        html_content = self.get_html(url)
+    def run(self, url: str, save_to_file: bool = False) -> str:
+        html_content: Optional[str] = self.get_html(url)
         if html_content:
-            product_info = self.parse_html(html_content)
+            product_info: Dict[str, Any] = self.parse_html(html_content)
             if save_to_file:
                 self.save_to_json(product_info)
             return json.dumps(product_info, ensure_ascii=False, indent=4)
 
 if __name__ == "__main__":
-    parser = WebPageParser(debug=False)
+    parser = WebPageParser(debug=True)
     json_data = parser.run("https://oxanakrengel.com/tvidovyi-kostyum-goluboi", save_to_file=True)
-    # print(json_data)
+    print(json_data)
