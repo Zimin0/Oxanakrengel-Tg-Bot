72649bc30d4cd93cab67c2f0c4b9a2099d02472b
Zimin0 <you@example.com>
2024-03-25 15:36:17 +0300

    Add some fields to models

--- django/service_project/order/admin.py
+++ django/service_project/order/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from order.models import BotOrder, PersonalData
+
+@admin.register(PersonalData)
+class PersonalDataAdmin(admin.ModelAdmin):
+    list_display = ('telegram_user_id', 'name', 'surname', 'email', 'phone_number')
+    search_fields = ('telegram_user_id', 'name', 'surname', 'email', 'phone_number')
+
+@admin.register(BotOrder)
+class BotOrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'personal_data', 'product_link', 'size', 'shipping_method', 'payment_method', 'price', 'status', 'creation_date', 'update_date')
+    list_filter = ('shipping_method', 'payment_method', 'status', 'creation_date')
+    search_fields = ('product_link', 'personal_data__name', 'personal_data__surname', 'personal_data__telegram_user_id')
+    raw_id_fields = ('personal_data',)
+    date_hierarchy = 'creation_date'
+    ordering = ('-creation_date',)
--- django/service_project/order/models.py
+++ django/service_project/order/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class PersonalData(models.Model):
+    telegram_user_id = models.CharField(verbose_name="Идентификатор пользователя telegram", max_length=100, blank=False, unique=True)
+    name = models.CharField(verbose_name="Имя", max_length=80, blank=False)
+    surname = models.CharField(verbose_name="Фамилия", max_length=80, blank=False)
+    address = models.TextField(verbose_name="Адрес", blank=True, null=True)
+    email = models.EmailField(verbose_name="Почта", blank=False)
+    phone_number = models.CharField(verbose_name="Номер телефона", max_length=20, blank=False)
+
+    def __str__(self):
+        return f"Пользователь - {self.telegram_user_id}"
+    
+    class Meta:
+        verbose_name = "Данные пользователя telegram"
+        verbose_name_plural = "Данные пользователя telegram"
+
+class BotOrder(models.Model):
+    SHIPPING_METHODS = (
+        ("delivery_moscow", "Доставка курьером по Москве"),
+        ("delivery_russia", "Доставка курьером по России"),
+        ("delivery_pickup", "Самовывоз")
+    )
+    PAYMENT_METHODS = (
+        ("card_ru", "Карта РФ "),
+        ("paypal", "PayPal"),
+    )
+    STATUSES = (
+        ('waiting_for_payment', 'Ожидает оплаты'),
+        ('delivery_in_progress', 'В процессе доставки'),
+        ('finished', 'Завершен'),
+        ('returned', 'Возврат')
+    ) 
+    personal_data = models.ForeignKey(PersonalData, on_delete=models.PROTECT, related_name="orders")
+    product_link = models.CharField(verbose_name="Ссылка на товар в магазине", max_length=300, blank=False)
+    size = models.IntegerField(verbose_name="Размер одежды", blank=False)
+    shipping_method = models.CharField(verbose_name="Метод доставки", max_length=50, choices=SHIPPING_METHODS)
+    payment_method = models.CharField(verbose_name="Метод оплаты", max_length=50, choices=PAYMENT_METHODS)
+    price = models.DecimalField(verbose_name="Цена", decimal_places=2, max_digits=10, blank=False)
+    status = models.CharField(verbose_name="Статус заказа", max_length=60, choices=STATUSES)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Заказ в telegram боте"
+        verbose_name_plural = "Заказы в telegram боте"
+        ordering = ['-creation_date']
+
+    def __str__(self):
+        return f"Заказ №{self.id}"
