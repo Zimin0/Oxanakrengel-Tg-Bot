fe5190bd96d17d449c258b46db5bab1db3327c5e
Zimin0 <you@example.com>
2024-03-27 16:28:39 +0300

    Fix bug with duplicated json phrase file

--- django/service_project/phrase_json/models.py
+++ django/service_project/phrase_json/models.py
@@ -1,16 +1,23 @@
 from django.db import models
 from django.core.exceptions import ValidationError
+from django.core.files.storage import default_storage
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 
 class BotPhrases(models.Model):
-    phrases = models.FileField(verbose_name="Файл с фразами в формате .json")
+    phrases = models.FileField(verbose_name="Файл с фразами в формате .json", upload_to='phrases/')
     updated_at = models.DateTimeField(verbose_name="Дата изменения", auto_now=True)
 
     def save(self, *args, **kwargs):
-        # Проверяем, существует ли уже запись в модели
-        if not self.pk and BotPhrases.objects.exists():
-            # Если запись существует, вызываем исключение
-            raise ValidationError('Невозможно создать более одной записи BotPhrases.')
-        return super(BotPhrases, self).save(*args, **kwargs)
+        # Удаление предыдущего файла
+        if self.pk:
+            try:
+                old_file = BotPhrases.objects.get(pk=self.pk).phrases
+                if old_file.name != self.phrases.name:
+                    old_file.delete(save=False)
+            except BotPhrases.DoesNotExist:
+                pass
+        super(BotPhrases, self).save(*args, **kwargs)
 
     def __str__(self):
         return f"Фразы бота | {self.updated_at.strftime('%Y-%m-%d %H:%M:%S')}"
