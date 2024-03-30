ee68a7110e8675a9729ba0e77677a1b8991202fa
Zimin0 <you@example.com>
2024-03-30 20:32:48 +0300

    Change db settings, add env file to django, add nginx conf

--- django/service_project/config/settings.py
+++ django/service_project/config/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('NAME_DB'),
+        'USER': os.getenv('USER_DB'),
+        'PASSWORD': os.getenv('PASSWORD_DB'),
+        'HOST': os.getenv('HOST_DB'),
+        'PORT': os.getenv('PORT_DB')
     }
 }
 
--- django/service_project/gunicorn.py
+++ django/service_project/gunicorn.py
@@ -1,7 +1,17 @@
 from multiprocessing import cpu_count
-from os import environ
+from dotenv import load_dotenv
+import os
 
-def get_workers():
-    return cpu_count()
+load_dotenv()
 
-bind = '0.0.0.0:' + 
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+bind = '0.0.0.0:' + os.getenv('PORT', '8000')
+max_requests = 1000
+worker_class = 'gevent'
+workers = cpu_count() # кол-во ядер == кол-ву воркеров
+env = {
+    'DJANGO_SETTINGS_MODULE': 'config.settings'
+}
+reload = True
+name = 'config'
