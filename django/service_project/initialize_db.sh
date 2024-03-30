26523e4a648503b98bc96cb52a5578017da51442
Zimin0 <you@example.com>
2024-03-30 21:38:50 +0300

    Add initialize.sh file

---
+++ django/service_project/initialize_db.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+#!/bin/sh
+
+# Загрузка переменных окружения из файла .env в текущий shell
+if [ -f .env ]; then
+  export $(cat .env | xargs)
+fi
+
+echo "Ожидание Постгрес."
+
+until PGPASSWORD="$POSTGRES_PASSWORD" psql -h "$HOST_DB" -p "$PORT_DB" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
+  >&2 echo "Постгрес недоступен..."
+  sleep 1
+done
+
+>&2 echo "Пострес работает."
+
+
+# Выход при любой ошибке
+set -e
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+
+# Запуск скрипта для создания суперпользователя
+python create_superuser.py --noinput
+
+exec "$@"
