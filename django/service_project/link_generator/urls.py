f49f21b77267eae79fda5d88479892c621588309
Zimin0 <you@example.com>
2024-04-01 16:58:41 +0300

    Fix bug with downloading json file

--- django/service_project/link_generator/urls.py
+++ django/service_project/link_generator/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from link_generator.views import generate_link
 
 urlpatterns = [
-    path('link/', generate_link, name='link'),
+    path('', generate_link, name='link'),
 ]
