c5ddad656dff9a60e45f2727f14b9e30cccc9c38
Zimin0 <you@example.com>
2024-04-03 14:51:23 +0300

    Try to fix bug in bash script

--- django/service_project/initialize_db.sh
+++ django/service_project/initialize_db.sh
@@ -1,28 +1,28 @@
 #!/bin/sh
 
-# Загрузка переменных окружения из файла .env в текущий shell
-if [ -f .env ]; then
-  set -a  # Автоматически экспортировать все переменные
-  . ./.env
-  set +a  # Выключить автоэкспорт
-fi
+# # Загрузка переменных окружения из файла .env в текущий shell
+# if [ -f .env ]; then
+#   set -a  # Автоматически экспортировать все переменные
+#   . ./.env
+#   set +a  # Выключить автоэкспорт
+# fi
 
-echo "Ожидание Постгрес."
+# echo "Ожидание Постгрес."
 
-until PGPASSWORD="$POSTGRES_PASSWORD" psql -h "$HOST_DB" -p "$PORT_DB" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
-  >&2 echo "Постгрес недоступен..."
-  sleep 1
-done
+# until PGPASSWORD="$POSTGRES_PASSWORD" psql -h "$HOST_DB" -p "$PORT_DB" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
+#   >&2 echo "Постгрес недоступен..."
+#   sleep 1
+# done
 
->&2 echo "Пострес работает."
+# >&2 echo "Пострес работает."
 
-# Выход при любой ошибке
-set -e
+# # Выход при любой ошибке
+# set -e
 
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
+# python manage.py makemigrations --noinput
+# python manage.py migrate --noinput
 
-# Запуск скрипта для создания суперпользователя
-python create_superuser.py --noinput
+# # Запуск скрипта для создания суперпользователя
+# python create_superuser.py --noinput
 
-exec "$@"
+# exec "$@"
