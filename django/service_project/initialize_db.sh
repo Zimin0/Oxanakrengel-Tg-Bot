1b87ae2123226253713d85d4e2799e166971776d
Zimin0 <you@example.com>
2024-04-06 19:46:46 +0300

    Add initialize of db

--- django/service_project/initialize_db.sh
+++ django/service_project/initialize_db.sh
@@ -1,28 +1,7 @@
 #!/bin/sh
 
-# # Загрузка переменных окружения из файла .env в текущий shell
-# if [ -f .env ]; then
-#   set -a  # Автоматически экспортировать все переменные
-#   . ./.env
-#   set +a  # Выключить автоэкспорт
-# fi
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 
-# echo "Ожидание Постгрес."
-
-# until PGPASSWORD="$POSTGRES_PASSWORD" psql -h "$HOST_DB" -p "$PORT_DB" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
-#   >&2 echo "Постгрес недоступен..."
-#   sleep 1
-# done
-
-# >&2 echo "Пострес работает."
-
-# # Выход при любой ошибке
-# set -e
-
-# python manage.py makemigrations --noinput
-# python manage.py migrate --noinput
-
-# # Запуск скрипта для создания суперпользователя
-# python create_superuser.py --noinput
-
-# exec "$@"
+# Запуск скрипта для создания суперпользователя
+python create_superuser.py --noinput
