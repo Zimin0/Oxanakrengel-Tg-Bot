efbb07aea02c7e624bb46639a107f107bbbb956e
Zimin0 <you@example.com>
2024-04-06 20:08:51 +0300

    Fix bug in bash script

--- django/service_project/initialize_db.sh
+++ django/service_project/initialize_db.sh
@@ -2,9 +2,6 @@
 
 export DJANGO_SETTINGS_MODULE=config.settings
 
-sleep 5
-
-python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 
 # Запуск скрипта для создания суперпользователя
