9f8a6326386fe702b368c55867457b038f193c42
Zimin0 <you@example.com>
2024-04-06 18:56:13 +0300

    Change django inner directory container

--- django/service_project/config/settings.py
+++ django/service_project/config/settings.py
@@ -24,7 +24,7 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "95.163.235.225"]
 
