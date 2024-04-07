41343910859579261587464cd408960fcaba787a
Zimin0 <you@example.com>
2024-04-08 02:32:06 +0300

    Change path to bot folder in bash script

--- tg_bot/setup_cron.sh
+++ tg_bot/setup_cron.sh
@@ -2,10 +2,10 @@
 crontab -l > mycron
 
 # Добавить задачу cron для очистки директории
-echo "0 0 * * * /root/oxana_bot_test/Oxanakrengel-Tg-Bot/tg_bot/clear_products_json.sh" >> mycron
+echo "0 0 * * * /bot/clear_products_json.sh" >> mycron
 
 # Добавить задачу cron для запуска скрипта Python каждую минуту
-echo "* * * * * python /root/oxana_bot_test/Oxanakrengel-Tg-Bot/tg_bot/httpx_requests/json_file.py" >> mycron
+echo "* * * * * python /bot/httpx_requests/json_file.py" >> mycron
 
 crontab mycron
 rm mycron
