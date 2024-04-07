c0dfefb9f3de577d4d16937ff21446680fb3709e
Zimin0 <you@example.com>
2024-04-08 02:01:49 +0300

    Change cron script

--- tg_bot/setup_cron.sh
+++ tg_bot/setup_cron.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
+crontab -l > mycron
 
 # Добавить задачу cron для очистки директории
-echo "0 0 * * * /root/oxana_bot_test/Oxanakrengel-Tg-Bot/tg_bot/clear_products_json.sh" > /etc/cron.d/telegram_bot_cron
+echo "0 0 * * * /root/oxana_bot_test/Oxanakrengel-Tg-Bot/tg_bot/clear_products_json.sh" >> mycron
 
 # Добавить задачу cron для запуска скрипта Python каждую минуту
-echo "* * * * * python /root/oxana_bot_test/Oxanakrengel-Tg-Bot/tg_bot/httpx_requests/json_file.py" >> /etc/cron.d/telegram_bot_cron
+echo "* * * * * python /root/oxana_bot_test/Oxanakrengel-Tg-Bot/tg_bot/httpx_requests/json_file.py" >> mycron
 
-# Права на файл для cron
-chmod 0644 /etc/cron.d/telegram_bot_cron
-
-# Применить изменения cron
-crontab /etc/cron.d/telegram_bot_cron
-
-# Запустить cron в фоне
-cron -f
+crontab mycron
+rm mycron
