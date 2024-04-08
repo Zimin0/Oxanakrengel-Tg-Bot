6fccd1b4c24c189cc8277fa8fb40167b22375bfa
Zimin0 <you@example.com>
2024-04-08 04:31:52 +0300

    Fix bug with cron

--- tg_bot/setup_cron.sh
+++ tg_bot/setup_cron.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 crontab -l > mycron
 # Добавить задачу cron для очистки директории
-echo "0 0 * * * /bot/clear_products_json.sh" >> mycron
+echo "0 0 * * * /bin/bash /bot/clear_products_json.sh" >> mycron
 # Добавить задачу cron для запуска скрипта Python каждую минуту
-echo "* * * * * /bot/upload_phrases_for_cron.sh" >> mycron
+echo "* * * * * /bin/bash /bot/upload_phrases_for_cron.sh" >> mycron
 crontab mycron
 rm mycron
