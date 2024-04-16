3a264933a17eb1da66e7d6222380b10e1fc42387
Zimin0 <you@example.com>
2024-04-16 23:20:52 +0300

    Add endpoint in api for user-settings

--- django/service_project/api/urls.py
+++ django/service_project/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PersonalDataViewSet, BotOrderViewSet, SupportRequestViewSet
+from .views import PersonalDataViewSet, BotOrderViewSet, SupportRequestViewSet, UserSettingViewSet
 
 router = DefaultRouter()
 router.register(r'personaldata', PersonalDataViewSet)
 router.register(r'botorder', BotOrderViewSet)
 router.register(r'supportrequest', SupportRequestViewSet)
+router.register(r'user-setting', UserSettingViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
--- django/service_project/api/views.py
+++ django/service_project/api/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from order.models import PersonalData, BotOrder
 from support.models import SupportRequest
-from api.serializers import PersonalDataSerializer, SupportRequestSerializer, BotOrderSerializer
+from user_setting.models import Setting
+from django_filters.rest_framework import DjangoFilterBackend
+from api.serializers import PersonalDataSerializer, SupportRequestSerializer, BotOrderSerializer, UserSettingSerializer
 
 class PersonalDataViewSet(viewsets.ModelViewSet):
     queryset = PersonalData.objects.all()
@@ -16,3 +18,9 @@ class BotOrderViewSet(viewsets.ModelViewSet):
 class SupportRequestViewSet(viewsets.ModelViewSet):
     queryset = SupportRequest.objects.all()
     serializer_class = SupportRequestSerializer 
+
+class UserSettingViewSet(viewsets.ModelViewSet):
+    queryset = Setting.objects.all()
+    serializer_class = UserSettingSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['slug']
