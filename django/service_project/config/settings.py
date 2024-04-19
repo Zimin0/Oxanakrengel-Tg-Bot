4ad6b1797d865bba443face043d6f90cbbeaf1f9
Zimin0 <you@example.com>
2024-04-19 23:53:00 +0300

    Add host to ALLOWED_HOSTS

--- django/service_project/config/settings.py
+++ django/service_project/config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "95.163.235.225", '127.0.0.1']
+ALLOWED_HOSTS = ["localhost", "79.174.80.95", "127.0.0.1", "oxanakrengel-tech.ru"]
 
 STATIC_URL = 'static/'
 STATIC_ROOT = 'static/'
