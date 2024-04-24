15e4c949056bcd163e3f69394d263a9bd8d27ed2
Zimin0 <you@example.com>
2024-04-24 13:02:49 +0300

    Improve code of creating links

--- config.py
+++ config.py
@@ -5,5 +5,9 @@ load_dotenv()
 
 PAYMENT_TEST_MODE = True
 
+MAX_TELEGRAM_ARG_LENGTH = 64 # максимальная длина агрумента, передаваемого в команду start=
+BOT_TELEGRAM_NAME = 'OxanaKrengelShopBot'
+SHOP_URL = 'https://oxanakrengel.com'
+
 TOKEN = os.getenv("BOT_TOKEN")
 DJANGO_URL = os.getenv("DJANGO_URL")
