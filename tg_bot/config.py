fb290ddee89cd256ea45073fb215d574a2df46ab
Zimin0 <you@example.com>
2024-05-07 12:48:27 +0300

    Chenge PAYMENT_TEST_MODE for test

--- tg_bot/config.py
+++ tg_bot/config.py
@@ -3,7 +3,7 @@ import os
 
 load_dotenv()
 
-PAYMENT_TEST_MODE = False
+PAYMENT_TEST_MODE = True
 DISABLE_VALIDATION = False
 
 MAX_TELEGRAM_ARG_LENGTH = 64 # максимальная длина агрумента, передаваемого в команду start=
